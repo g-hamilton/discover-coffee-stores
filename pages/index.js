a9cd840dcb2ed37ef6e3f058b42fdfb8dca7f703
g-hamilton <greghamilton1@gmail.com>
2022-01-07 12:52:43 +0000

    card component

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 
 import styles from "../styles/Home.module.css";
 import Banner from "../components/banner";
+import Card from "../components/card";
 
 export default function Home() {
   const handleOnBannerBtnClick = () => {
@@ -21,18 +22,26 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <div className={styles.banner}>
-          <Banner
-            buttonText="View shops nearby"
-            handleOnClick={handleOnBannerBtnClick}
-          />
-        </div>
         <div className={styles.heroImage}>
           <Image
             src="/static/hero-image.png"
             alt="Lady drinking coffee"
             width={700}
             height={400}
+          />
+        </div>
+        <div className={styles.banner}>
+          <Banner
+            buttonText="View shops nearby"
+            handleOnClick={handleOnBannerBtnClick}
+          />
+        </div>
+        <div className={styles.cardLayout}>
+          <Card
+            href="/coffee-store/darkHorse"
+            name="DarkHorse Coffee"
+            imgUrl="/static/hero-image.png"
+            className={styles.card}
           />
         </div>
       </main>
