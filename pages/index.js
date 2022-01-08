6b5e894bfd6f4d79b49aaa88f8a77d456a40976f
g-hamilton <greghamilton1@gmail.com>
2022-01-08 14:16:57 +0000

    foursquare api for homepage data

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,15 @@ import styles from "../styles/Home.module.css";
 import Banner from "../components/banner";
 import Card from "../components/card";
 
-import coffeeStoresData from "../data/coffee-stores.json";
+// import coffeeStoresData from "../data/coffee-stores.json";
+import { fetchCoffeeStores } from "../lib/coffee-stores";
 
 export async function getStaticProps(context) {
-  // const coffeeStores = await fetchCoffeeStores();
+  const coffeeStores = await fetchCoffeeStores();
+
   return {
     props: {
-      coffeeStores: coffeeStoresData,
+      coffeeStores,
     }, // will be passed to the page component as props
   };
 }
@@ -49,7 +51,7 @@ export default function Home(props) {
         </div>
         {props.coffeeStores.length > 0 && (
           <>
-            <h2 className={styles.heading2}>Toronto coffee shops</h2>
+            <h2 className={styles.heading2}>London coffee shops</h2>
             <div className={styles.cardLayout}>
               {props.coffeeStores.map((coffeeStore) => {
                 return (
@@ -57,7 +59,7 @@ export default function Home(props) {
                     key={coffeeStore.id}
                     href={`/coffee-store/${coffeeStore.id}`}
                     name={coffeeStore.name}
-                    imgUrl={coffeeStore.imgUrl}
+                    imgUrl={`https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80`}
                     className={styles.card}
                   />
                 );
