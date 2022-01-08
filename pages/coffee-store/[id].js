0dd84f39ab9ab7542b809449cf59cafee875eaad
g-hamilton <greghamilton1@gmail.com>
2022-01-08 10:30:49 +0000

    SSG with fallback. More structure

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,9 +1,55 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
+import Head from "next/head";
 
-const CoffeeStore = () => {
+import coffeeStoresData from "../../data/coffee-stores.json";
+
+export function getStaticPaths() {
+  const paths = coffeeStoresData.map((coffeeStore) => {
+    return {
+      params: {
+        id: coffeeStore.id.toString(),
+      },
+    };
+  });
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export function getStaticProps(staticProps) {
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find((coffeeStore) => {
+        return coffeeStore.id.toString() === staticProps.params.id;
+      }),
+    },
+  };
+}
+
+const CoffeeStore = (props) => {
   const router = useRouter();
 
-  return <div>Coffee Store Page {router.query.id}</div>;
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  const { address, name, neighbourhood } = props.coffeeStore;
+
+  return (
+    <div>
+      <Head>
+        <title>{name}</title>
+      </Head>
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+      <p>{address}</p>
+      <p>{name}</p>
+      <p>{neighbourhood}</p>
+    </div>
+  );
 };
 
 export default CoffeeStore;
