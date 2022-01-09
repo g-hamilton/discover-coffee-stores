cd92fb5ebc09493a211743c5890aa461b0a6832b
g-hamilton <greghamilton1@gmail.com>
2022-01-09 14:06:06 +0000

    coffee store context provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import { createContext } from "react";
+
+import "../styles/globals.css";
+
+const coffeeStoreContext = createContext();
+
+const CoffeeStoreContextProvider = ({ children }) => {
+  const initialState = {
+    latLong: "",
+    coffeeStores: [],
+  };
+  return (
+    <coffeeStoreContext.Provider value={{ state: initialState }}>
+      {children}
+    </coffeeStoreContext.Provider>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <CoffeeStoreContextProvider>
+      <Component {...pageProps} />
+    </CoffeeStoreContextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
