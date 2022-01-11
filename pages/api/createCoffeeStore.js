58e345d65ef6cee8d0261614e72be4d96784e7a4
g-hamilton <greghamilton1@gmail.com>
2022-01-11 09:03:24 +0000

    create coffee store with error handling

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -5,11 +5,91 @@ const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
 
 const table = base("coffee-stores");
 
-const createCoffeeStore = (req, res) => {
+const createCoffeeStore = async (req, res) => {
   if (req.method === "POST") {
-    res.status(200).json({ message: "Hi there!" });
-  } else {
-    res.json({ message: "Sorry I only accept POST requests!" });
+    const { id, name, address, neighbourhood, voting, imgUrl } = req.body;
+
+    try {
+      // validate
+      if (!id) {
+        return res
+          .status(400)
+          .json({ message: "Unable to complete: Missing id field" });
+      }
+
+      // find a record
+      const foundCoffeeStoreRecords = await table
+        .select({ filterByFormula: `id=${id}` })
+        .firstPage(); // returns an array (may be empty if no matching record)
+
+      if (
+        foundCoffeeStoreRecords &&
+        foundCoffeeStoreRecords.length &&
+        foundCoffeeStoreRecords.length !== 0
+      ) {
+        // transform the records if found
+        const records = foundCoffeeStoreRecords.map((record) => {
+          return {
+            ...record.fields,
+          };
+        });
+
+        // return the transformed records
+        res.json({ message: "Record already exists", records });
+      } else {
+        // create a new record if not found
+
+        // validate
+        if (!id) {
+          return res
+            .status(400)
+            .json({ message: "Unable to complete: Missing id field" });
+        }
+        if (!name) {
+          return res
+            .status(400)
+            .json({ message: "Unable to complete: Missing name field" });
+        }
+        if (!address) {
+          return res
+            .status(400)
+            .json({ message: "Unable to complete: Missing address field" });
+        }
+        if (!imgUrl) {
+          return res
+            .status(400)
+            .json({ message: "Unable to complete: Missing imgUrl field" });
+        }
+
+        const createRecords = await table.create([
+          {
+            fields: {
+              id,
+              name,
+              address,
+              neighbourhood,
+              voting,
+              imgUrl,
+            },
+          },
+        ]);
+
+        // transform the records if created successfully
+        const records = createRecords.map((record) => {
+          return {
+            ...record.fields,
+          };
+        });
+
+        // return the transformed records
+        res.json({ message: "Record created", records });
+      }
+    } catch (err) {
+      console.error(err);
+      res
+        .status(500)
+        .json({ message: "Oops something went wrong", error: err });
+    }
   }
 };
 
